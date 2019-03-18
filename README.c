003ce87f8ea225bd2faf79429ba06a509776edd3
vusanthiya <vusanthiya@gmail.com>
2019-03-18 11:27:24 +0530

    Update README.c

--- README.c
+++ README.c
@@ -6,7 +6,7 @@ printf("Enter the string 1:");
 scanf("%s",str1);
 printf("Enter the string 2:");
 scanf("%s",str2);
-for(i=0;str1[i]==str2[i]&&str1!=='\0';i++)
+for(i=0;str1[i]==str2[i]&&str1[i]!=='\0';i++)
 if(str1[i]<str2[i])
 {
 printf("less");
