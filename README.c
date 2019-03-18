8b0780bb7240712f36217a90e048682291ba85c8
vusanthiya <vusanthiya@gmail.com>
2019-03-18 11:26:59 +0530

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,23 @@
+void main()
+{
+char str1[50],str2[50];
+int i;
+printf("Enter the string 1:");
+scanf("%s",str1);
+printf("Enter the string 2:");
+scanf("%s",str2);
+for(i=0;str1[i]==str2[i]&&str1!=='\0';i++)
+if(str1[i]<str2[i])
+{
+printf("less");
+}
+elseif(str1[i]>str2[i])
+{
+printf("greater");
+}
+else
+{
+printf(str2);
+}
+getch();
+}
